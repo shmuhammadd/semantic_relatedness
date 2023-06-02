d8ae1cb9dedcde6dd0ba7ecfc3105b6bd7596ac3
shamsuddeen muhammad <shmuhammad.csc@buk.edu.ng>
2023-06-02 20:01:14 +0100

    README

---
+++ relatedness_script.sh
@@ -0,0 +1,7 @@
+python3 scripts/semantic_relatedness.py \
+  -i data/sentence.txt \
+  -s data/stopwords_hausa.csv \
+  -o results \
+  --maximum_matches 3 \
+  --clean_sentences \
+  --remove_stopwords
